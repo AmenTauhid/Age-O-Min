acb15d2b084a6abebe39390b4c23885787938238
Amen <aymantauhid2@gmail.com>
2022-03-05 01:25:04 +0530

    Option to select date from the pop-up calendar implemented.

--- app/src/main/java/com/amen/birthyeartomincalc/MainActivity.kt
+++ app/src/main/java/com/amen/birthyeartomincalc/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.amen.birthyeartomincalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var txtSelectedDate : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCalendarPop : Button = findViewById(R.id.CalendarPop)
+        txtSelectedDate = findViewById(R.id.SelectedDate)
+        btnCalendarPop.setOnClickListener {
+            clickCalendarPop()
+        }
+    }
+
+    private fun clickCalendarPop(){
+
+
+        val calendarPop = Calendar.getInstance()
+        val year = calendarPop.get(Calendar.YEAR)
+        val month = calendarPop.get(Calendar.MONTH)
+        val day = calendarPop.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _, chosenYear, chosenMonth, chosenDayOfMonth ->
+                val selectedDate = "$chosenDayOfMonth.$chosenMonth.$chosenYear"
+                txtSelectedDate?.text = selectedDate
+            },
+            year,
+            month,
+            day
+        ).show()
+
+
+
+
     }
 }
