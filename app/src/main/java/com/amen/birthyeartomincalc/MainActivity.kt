d97f1a15bbe4faafedb6c164eb89d21470b87f1c
Amen <aymantauhid2@gmail.com>
2022-03-05 14:09:02 +0530

    Final implementation to show the user their age in minutes successful. Also certain error regarding android.SimpleDateFormat occurred which caused app crashing whenever the date was picked due API level requirement. This issue was solved by using java.SimpleDateFormat module.
    For future: App will be optimized more and certain new features will be implemented later on.

--- app/src/main/java/com/amen/birthyeartomincalc/MainActivity.kt
+++ app/src/main/java/com/amen/birthyeartomincalc/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.amen.birthyeartomincalc
 
+import android.icu.text.SimpleDateFormat
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private var txtSelectedDate : TextView? = null
+    private var txtDateInMin : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        txtDateInMin = findViewById(R.id.DateInMIn)
         val btnCalendarPop : Button = findViewById(R.id.CalendarPop)
         txtSelectedDate = findViewById(R.id.SelectedDate)
         btnCalendarPop.setOnClickListener {
@@ -29,20 +32,36 @@ class MainActivity : AppCompatActivity() {
         val year = calendarPop.get(Calendar.YEAR)
         val month = calendarPop.get(Calendar.MONTH)
         val day = calendarPop.get(Calendar.DAY_OF_MONTH)
-
-        DatePickerDialog(
+        val datePickerDialog = DatePickerDialog(
             this,
-            DatePickerDialog.OnDateSetListener { _, chosenYear, chosenMonth, chosenDayOfMonth ->
-                val selectedDate = "$chosenDayOfMonth.$chosenMonth.$chosenYear"
+            { _, chosenYear, chosenMonth, chosenDayOfMonth ->
+                val selectedDate = "$chosenDayOfMonth.${chosenMonth + 1}.$chosenYear"
                 txtSelectedDate?.text = selectedDate
+
+                val dataFormat = java.text.SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH)
+
+                val reqDate = dataFormat.parse(selectedDate)
+
+                reqDate?.let {
+                    val reqDateInMin = reqDate.time/60000
+
+                    val currentDate = dataFormat.parse((dataFormat.format(System.currentTimeMillis())))
+
+                    currentDate?.let {
+                        val currentDateInMin = currentDate.time/60000
+
+                        val diffInMin = currentDateInMin - reqDateInMin
+
+                        txtDateInMin?.text = diffInMin.toString()
+                    }
+
+                }
             },
             year,
             month,
             day
-        ).show()
-
-
-
-
+        )
+        datePickerDialog.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        datePickerDialog.show()
     }
 }
